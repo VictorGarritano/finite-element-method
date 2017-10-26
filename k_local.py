110b387cc0aef28234a8fdbe18906547016998a4
Victor Garritano <victortaichou@gmail.com>
2017-10-26 20:05:43 -0200

    building element matrix

---
+++ k_local.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def gradient_interpolator(eta_bold):
+    """Matrix B"""
+    ksi, eta = eta_bold[0], eta_bold[1]
+    grad_phi1 = [-0.25*(1-eta), -0.25*(1-ksi)]
+    grad_phi2 = [0.25*(1-eta), -0.25*(1+ksi)]
+    grad_phi3 = [0.25*(1+eta), 0.25*(1+ksi)]
+    grad_phi4 = [-0.25*(1+eta), 0.25*(1-ksi)]
+
+    return np.array([
+        grad_phi1,
+        grad_phi2,
+        grad_phi3,
+        grad_phi4
+    ])
+
+jacobian = lambda x, y: x.dot(y)
+gamma = lambda j: np.linalg.inv(j)
+det_J = lambda j: np.linalg.det(j)
+
+gauss_points = [[-np.sqrt(3)/3,-np.sqrt(3)/3],
+                [np.sqrt(3)/3, -np.sqrt(3)/3],
+                [-np.sqrt(3)/3, np.sqrt(3)/3],
+                [np.sqrt(3)/3, np.sqrt(3)/3]]
+
+def build_local_k(M, Q):
+    K_e = np.zeros((4,4))
+    for p in gauss_points:
+        B = gradient_interpolator(p)
+        J = jacobian(M, B)
+        G = gamma(J)
+        D = det_J(J)
+        K_e += B.dot(G.T).dot(Q).dot(G).dot(B.T)*D
+    return K_e
+
+if __name__ == '__main__':
+    M = np.array([
+        [0,0],
+        [1,0],
+        [1,1],
+        [0,1]
+    ]).T
+
+    Q = np.array([
+        [1,1],
+        [1,1]
+    ])
+    print(build_local_k(M,Q))
